7265975b4db051ed357730605097ffa3c1a044b3
ShadowAmy97 <82647376+ShadowAmy97@users.noreply.github.com>
2022-05-03 20:57:39 +0200

    cambios register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,10 +20,22 @@ class UserController extends AbstractController
     }
 
     #[Route('/register', name: 'register')]
-    public function register(Request $register): Response
+    public function register(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user->setActive(false);
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
 
         return $this->render('user/register.html.twig', [
             'title' => 'Register','form'=>$form->createView()
