47742be3447aa1a6b5defb2451d86d26a4cb7936
tecnicolopeza <tecnicolopeza@gmail.com>
2022-05-15 20:21:38 +0200

    img y doc publicaciones hecho

--- src/Twig/CustomExtension.php
+++ src/Twig/CustomExtension.php
@@ -12,7 +12,8 @@ class CustomExtension extends AbstractExtension
 {
     protected $doctrine;
 
-    function __construct(PersistenceManagerRegistry $doctrine) {
+    function __construct(PersistenceManagerRegistry $doctrine)
+    {
         $this->doctrine = $doctrine;
     }
 
@@ -23,6 +24,7 @@ class CustomExtension extends AbstractExtension
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
             new TwigFilter('userById', [$this, 'userByIdFilter']),
+            new TwigFilter('timeAgo', [$this, 'timeAgoFilter']),
         ];
     }
 
@@ -33,12 +35,87 @@ class CustomExtension extends AbstractExtension
         ];
     }
 
-    public function userByIdFilter($user_id){
+    public function userByIdFilter($user_id)
+    {
 
         $entityManager = $this->doctrine->getManager();
         $user_repository = $entityManager->getRepository(User::class);
         $user = $user_repository->find($user_id);
 
         return $user;
+    }
+
+    /**
+     * @Route("Route", name="RouteName")
+     */
+    public function timeAgoFilter($timestamp)
+    {
+        $time_ago = strtotime($timestamp);
+        $current_time = time();
+        $time_difference = $current_time - $time_ago;
+
+        $seconds = $time_difference;
+        $minutes = round($seconds / 60);
+        $hours = round($seconds / 3600);
+        $days = round($seconds / 86400);
+        $weeks = round($seconds / 604800);
+        $months = round($seconds / 2629440);
+        $years = round($seconds / 31553280);
+
+        if ($seconds <= 60) {
+
+            return "just now";
+
+        } else if ($minutes <= 60) {
+
+            if ($minutes == 1) {
+                return "one minute ago";
+            } else {
+                return "$minutes minutes ago";
+            }
+
+        } else if ($hours <= 24) {
+
+            if ($hours == 1) {
+                return "an hour ago";
+            } else {
+                return "$hours hours ago";
+            }
+
+        } else if ($days <= 7) {
+
+            if ($days == 1) {
+                return "yesterday";
+            } else {
+                return "$days days ago";
+            }
+
+        } else if($weeks >= 4.3){
+
+            if ($weeks == 1) {
+                return "a week ago";
+            }else{
+                return "$weeks weeks ago";
+            }
+
+        } else if($months <= 12){
+
+            if ($months == 1) {
+                return "a month ago";
+            }else{
+                return "$months months ago";
+            }
+
+        } else {
+
+            if($years == 1){
+                return "a year ago";
+            }else{
+                return "$years years ago";
+            }
+
+        }
+
+
     }
 }
